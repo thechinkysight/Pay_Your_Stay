ae38c2b2ca5671904930b2f77d87e0b903967adf
The Chinky Sight <thechinkysight@gmail.com>
2024-02-18 11:08:36 +0545

    Added functionality to show error message on text fields when text fields are empty.
    I've added two functions:
    
    1. showErrorWhenTextFieldIsEmpty:
    
    This function simply encapsulates the following and core functions for the functionality.
    
    2. showErrorWhenTextFieldIsEmptyAfterFocus:
    
    This function shows error message when text field is left empty after being focused. It's because, by default, the text fields are empty. And when user comes to enter a value and backs off, the text fields still remains empty which not expected behavior so it cautions the user.
    
    The text fields comes empty, but it's not expected to leave empty.
    
    3. showErrorWhenTextFieldIsEmptyAfterTextChange:
    
    This function shows error message when the user leaves the text field empty after clearing the previous data.

--- app/src/main/java/thechinkysight/app/payyourstay/screens/CalculatorFragment.kt
+++ app/src/main/java/thechinkysight/app/payyourstay/screens/CalculatorFragment.kt
@@ -1,12 +1,17 @@
 package thechinkysight.app.payyourstay.screens
 
 import android.os.Bundle
+import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import androidx.annotation.StringRes
+import androidx.core.widget.doAfterTextChanged
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.google.android.material.textfield.TextInputEditText
 import thechinkysight.app.payyourstay.R
 import thechinkysight.app.payyourstay.databinding.FragmentCalculatorBinding
 import thechinkysight.app.payyourstay.viewmodels.CalculatorViewModel
@@ -14,19 +19,100 @@ import thechinkysight.app.payyourstay.viewmodels.CalculatorViewModel
 
 class CalculatorFragment : Fragment() {
 
-    private lateinit var _binding: FragmentCalculatorBinding
-    private val _calculatorViewModel: CalculatorViewModel by viewModels()
+    private lateinit var binding: FragmentCalculatorBinding
+    private val calculatorViewModel: CalculatorViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_calculator, container, false)
-        _binding.calculatorViewModel = _calculatorViewModel
-        _binding.lifecycleOwner = viewLifecycleOwner
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_calculator, container, false)
+        binding.calculatorViewModel = calculatorViewModel
+        binding.calculatorFragment = this
+        binding.lifecycleOwner = viewLifecycleOwner
 
-        return _binding.root
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        showErrorWhenTextFieldIsEmpty(
+            binding.previousElecMeterReadingTextField.editText,
+            R.string.error_message_for_empty_previous_reading
+        )
+        showErrorWhenTextFieldIsEmpty(
+            binding.currentElecMeterReadingTextField.editText,
+            R.string.error_message_for_empty_current_reading
+        )
+        showErrorWhenTextFieldIsEmpty(
+            binding.electricityRatePerUnitTextField.editText,
+            R.string.error_message_for_empty_electricity_rate
+        )
+
+
+
+        showErrorWhenTextFieldIsEmpty(
+            binding.waterFeeTextField.editText,
+            R.string.error_message_for_empty_water_fee
+        )
+        showErrorWhenTextFieldIsEmpty(
+            binding.garbageFeeTextField.editText,
+            R.string.error_message_for_empty_garbage_fee
+        )
+
+        
+
+        showErrorWhenTextFieldIsEmpty(
+            binding.rentTextField.editText,
+            R.string.error_message_for_empty_rent
+        )
+    }
+
+    private fun showErrorWhenTextFieldIsEmpty(
+        textInputEditText: EditText?,
+        @StringRes errorMessageId: Int
+    ) {
+        showErrorWhenTextFieldIsEmptyAfterFocus(textInputEditText, errorMessageId)
+        showErrorWhenTextFieldIsEmptyAfterTextChange(textInputEditText, errorMessageId)
+    }
+
+    private fun showErrorWhenTextFieldIsEmptyAfterFocus(
+        textInputEditText: EditText?,
+        @StringRes errorMessageId: Int
+    ) {
+
+        textInputEditText?.setOnFocusChangeListener { v: View, hasFocus: Boolean ->
+
+            if (!hasFocus) {
+                val isTextFieldEmpty: Boolean = (v as TextInputEditText).text?.isEmpty() == true
+                if (isTextFieldEmpty) {
+                    v.error = getString(errorMessageId)
+                } else {
+                    v.error = null
+                }
+            }
+
+        }
+
+    }
+
+
+    private fun showErrorWhenTextFieldIsEmptyAfterTextChange(
+        textInputEditText: EditText?,
+        @StringRes errorMessageId: Int
+    ) {
+        textInputEditText?.doAfterTextChanged { text: Editable? ->
+
+            if (text.toString().isEmpty()) {
+                textInputEditText.error =
+                    getString(errorMessageId)
+            } else {
+                textInputEditText.error = null
+            }
+
+        }
     }
 
 }
