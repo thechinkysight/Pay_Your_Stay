510b22848014eb000e460a257ffaa5fa7c9fe3c5
The Chinky Sight <thechinkysight@gmail.com>
2024-03-27 12:36:03 +0545

    Added leading icons for respective text fields and their content descriptions

--- app/src/main/java/thechinkysight/app/payyourstay/MainActivity.kt
+++ app/src/main/java/thechinkysight/app/payyourstay/MainActivity.kt
@@ -13,16 +13,17 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import thechinkysight.app.payyourstay.ui.theme.PayYourStayTheme
 
@@ -71,19 +72,31 @@ private fun ElectricityDataInputTextFields(modifier: Modifier = Modifier) {
     Column {
 
         DataInputTextField(
-            modifier = modifier, placeHolderStringResourceId = R.string.previous_elec_meter_reading
+            modifier = modifier, placeHolderStringResourceId = R.string.previous_elec_meter_reading,
+            leadingIconContent = mapOf(
+                "drawableId" to R.drawable.bolt_24,
+                "contentDescription" to R.string.previous_elec_reading_icon_content_description
+            ),
         )
 
         Spacer(modifier = Modifier.height(40.dp))
 
         DataInputTextField(
-            modifier = modifier, placeHolderStringResourceId = R.string.current_elec_meter_reading
+            modifier = modifier, placeHolderStringResourceId = R.string.current_elec_meter_reading,
+            leadingIconContent = mapOf(
+                "drawableId" to R.drawable.bolt_24,
+                "contentDescription" to R.string.current_elec_reading_icon_content_description
+            ),
         )
 
         Spacer(modifier = Modifier.height(40.dp))
 
         DataInputTextField(
-            modifier = modifier, placeHolderStringResourceId = R.string.electricity_rate_per_unit
+            modifier = modifier, placeHolderStringResourceId = R.string.electricity_rate_per_unit,
+            leadingIconContent = mapOf(
+                "drawableId" to R.drawable.payment_24,
+                "contentDescription" to R.string.electricity_rate_icon_content_description
+            ),
         )
     }
 
@@ -92,15 +105,33 @@ private fun ElectricityDataInputTextFields(modifier: Modifier = Modifier) {
 
 @Composable
 private fun OtherUtilitiesDataInputTextFields(modifier: Modifier = Modifier) {
-    DataInputTextField(modifier = modifier, placeHolderStringResourceId = R.string.water_fee)
+    DataInputTextField(
+        modifier = modifier, placeHolderStringResourceId = R.string.water_fee,
+        leadingIconContent = mapOf(
+            "drawableId" to R.drawable.water_drop_24,
+            "contentDescription" to R.string.water_fee_icon_content_description
+        ),
+    )
     Spacer(modifier = Modifier.height(40.dp))
-    DataInputTextField(modifier = modifier, placeHolderStringResourceId = R.string.garbage_fee)
+    DataInputTextField(
+        modifier = modifier, placeHolderStringResourceId = R.string.garbage_fee,
+        leadingIconContent = mapOf(
+            "drawableId" to R.drawable.delete_24,
+            "contentDescription" to R.string.garbage_fee_icon_content_description
+        ),
+    )
 }
 
 @Composable
 private fun RentDataInputTextFields(modifier: Modifier = Modifier) {
     DataInputTextField(
-        modifier = modifier, placeHolderStringResourceId = R.string.rent, imeAction = ImeAction.Done
+        modifier = modifier,
+        placeHolderStringResourceId = R.string.rent,
+        leadingIconContent = mapOf(
+            "drawableId" to R.drawable.payment_24,
+            "contentDescription" to R.string.rent_icon_content_description
+        ),
+        imeAction = ImeAction.Done
     )
 }
 
@@ -108,12 +139,19 @@ private fun RentDataInputTextFields(modifier: Modifier = Modifier) {
 private fun DataInputTextField(
     modifier: Modifier,
     @StringRes placeHolderStringResourceId: Int,
+    leadingIconContent: Map<String, Int>,
     imeAction: ImeAction = ImeAction.Next
 ) {
     OutlinedTextField(modifier = modifier,
         value = "",
         onValueChange = {},
         singleLine = true,
+        leadingIcon = {
+            Icon(
+                painter = painterResource(id = leadingIconContent["drawableId"]!!),
+                contentDescription = stringResource(id = leadingIconContent["contentDescription"]!!)
+            )
+        },
         keyboardOptions = KeyboardOptions.Default.copy(
             keyboardType = KeyboardType.Decimal, imeAction = imeAction
         ),
@@ -124,10 +162,10 @@ private fun DataInputTextField(
         })
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PayYourStayTheme {
-        PayYourStayApp()
-    }
-}
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    PayYourStayTheme {
+//        PayYourStayApp()
+//    }
+//}
