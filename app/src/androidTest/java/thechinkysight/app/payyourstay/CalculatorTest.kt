39af720ab19443680f256ee150fd3d1ffa1a55b1
The Chinky Sight <thechinkysight@gmail.com>
2024-04-18 21:03:36 +0545

    Refactored instrument tests for Calculator page to use thingUnderTest_Action_ResultOfAction naming convention

--- app/src/androidTest/java/thechinkysight/app/payyourstay/CalculatorTest.kt
+++ app/src/androidTest/java/thechinkysight/app/payyourstay/CalculatorTest.kt
@@ -8,39 +8,97 @@ import androidx.compose.ui.test.hasClickAction
 import androidx.compose.ui.test.hasSetTextAction
 import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 import thechinkysight.app.payyourstay.ui.theme.PayYourStayTheme
 
+@RunWith(AndroidJUnit4::class)
 class CalculatorTest {
 
     @get: Rule
     val composeTestRule = createAndroidComposeRule<ComponentActivity>()
 
-    @Test
-    fun areAllComponentsPresent() {
-
+    @Before
+    fun setUp() {
         composeTestRule.setContent {
             PayYourStayTheme {
                 PayYourStayApp(modifier = Modifier.verticalScroll(rememberScrollState()))
             }
         }
+    }
 
-        composeTestRule.onNode(hasSetTextAction() and hasText(composeTestRule.activity.getString(R.string.previous_elec_meter_reading)))
-            .assertExists()
-        composeTestRule.onNode(hasSetTextAction() and hasText(composeTestRule.activity.getString(R.string.current_elec_meter_reading)))
-            .assertExists()
-        composeTestRule.onNode(hasSetTextAction() and hasText(composeTestRule.activity.getString(R.string.electricity_rate_per_unit)))
-            .assertExists()
 
-        composeTestRule.onNode(hasSetTextAction() and hasText(composeTestRule.activity.getString(R.string.water_fee)))
-            .assertExists()
-        composeTestRule.onNode(hasSetTextAction() and hasText(composeTestRule.activity.getString(R.string.garbage_fee)))
-            .assertExists()
+    @Test
+    fun calculatorPage_PreviousElecMeterReadingComponent_Exist() {
+        composeTestRule.onNode(
+            hasSetTextAction() and hasText(
+                composeTestRule.activity.getString(
+                    R.string.previous_elec_meter_reading
+                )
+            )
+        ).assertExists()
+    }
 
-        composeTestRule.onNode(hasSetTextAction() and hasText(composeTestRule.activity.getString(R.string.rent)))
-            .assertExists()
+    @Test
+    fun calculatorPage_CurrentElecMeterReadingComponent_Exist() {
+        composeTestRule.onNode(
+            hasSetTextAction() and hasText(
+                composeTestRule.activity.getString(
+                    R.string.current_elec_meter_reading
+                )
+            )
+        ).assertExists()
+    }
 
+    @Test
+    fun calculatorPage_ElectricityRatePerUnitComponent_Exist() {
+        composeTestRule.onNode(
+            hasSetTextAction() and hasText(
+                composeTestRule.activity.getString(
+                    R.string.electricity_rate_per_unit
+                )
+            )
+        ).assertExists()
+    }
+
+    @Test
+    fun calculatorPage_WaterFeeComponent_Exist() {
+        composeTestRule.onNode(
+            hasSetTextAction() and hasText(
+                composeTestRule.activity.getString(
+                    R.string.water_fee
+                )
+            )
+        ).assertExists()
+    }
+
+    @Test
+    fun calculatorPage_GarbageFeeComponent_Exist() {
+        composeTestRule.onNode(
+            hasSetTextAction() and hasText(
+                composeTestRule.activity.getString(
+                    R.string.garbage_fee
+                )
+            )
+        ).assertExists()
+    }
+
+    @Test
+    fun calculatorPage_RentComponent_Exist() {
+        composeTestRule.onNode(
+            hasSetTextAction() and hasText(
+                composeTestRule.activity.getString(
+                    R.string.rent
+                )
+            )
+        ).assertExists()
+    }
+
+    @Test
+    fun calculatorPage_CalculateButton_Exist() {
         composeTestRule.onNode(
             hasClickAction() and hasText(
                 composeTestRule.activity.getString(R.string.calculate).uppercase()
@@ -48,6 +106,5 @@ class CalculatorTest {
         ).assertExists()
     }
 }
-
 
 
